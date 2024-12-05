42edba8fd8aff8ac00a7ae1f0ec5de5709d9a529
James Stine <james.stine@okstate.edu>
2024-12-05 09:11:49 -0600

    forgot header on TRNG file

--- tests/entropy/Base_entropy/TRNG.sv
+++ tests/entropy/Base_entropy/TRNG.sv
@@ -1,3 +1,30 @@
+///////////////////////////////////////////
+// TRNG.sv
+//
+// Written: kelvin.tran@okstate.edu, james.stine@okstate.edu
+// Created: 5 November 2024
+//
+// Purpose: RISCV main TRNG
+//
+// A component of the CORE-V-WALLY configurable RISC-V project.
+// https://github.com/openhwgroup/cvw
+// 
+// Copyright (C) 2021-24 Harvey Mudd College & Oklahoma State University
+//
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+//
+// Licensed under the Solderpad Hardware License v 2.1 (the “License”); you may not use this file 
+// except in compliance with the License, or, at your option, the Apache License version 2.0. You 
+// may obtain a copy of the License at
+//
+// https://solderpad.org/licenses/SHL-2.1/
+//
+// Unless required by applicable law or agreed to in writing, any work distributed under the 
+// License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+// either express or implied. See the License for the specific language governing permissions 
+// and limitations under the License.
+////////////////////////////////////////////////////////////////////////////////////////////////
+
 module trng #(parameter NUM_CELLS=3, parameter NUM_INV_START=5, parameter SIM_MODE=0) (
   input  logic       clk,
   input  logic       rst,
